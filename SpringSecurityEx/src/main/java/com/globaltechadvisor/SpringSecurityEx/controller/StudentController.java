571a6c710d5a95a981b93e27478a324e271ea278
atanusenstudy <atanusenstudy@gmail.com>
2025-08-18 13:42:17 +0530

    Initial working code

---
+++ SpringSecurityEx/src/main/java/com/globaltechadvisor/SpringSecurityEx/controller/StudentController.java
@@ -0,0 +1,48 @@
+package com.globaltechadvisor.SpringSecurityEx.controller;
+
+
+import com.globaltechadvisor.SpringSecurityEx.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    //Just for testing It
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request) {
+        return "Hello World "+request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request) {
+        return "Atanu "+request.getSession().getId();
+    }
+
+    List<Student> students=new ArrayList<>(List.of(
+            new Student(1,"Atanu",".Net-Angular"),
+            new Student(2,"Seb","Angular")
+    ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student) {
+        students.add(student);
+    }
+}
---
+++ SpringSecurityEx/src/main/java/com/globaltechadvisor/SpringSecurityEx/service/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.globaltechadvisor.SpringSecurityEx.service;
+
+import com.globaltechadvisor.SpringSecurityEx.dao.UserRepo;
+import com.globaltechadvisor.SpringSecurityEx.model.User;
+import com.globaltechadvisor.SpringSecurityEx.model.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user= repo.findByUsername(username);
+
+        if (user==null) {
+            System.out.println("User 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+        return new UserPrincipal(user);
+    }
+
+}
